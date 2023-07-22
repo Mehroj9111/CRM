63f6c8c45c80d8910df5eeacb35c0793a1637889
Mehroj9111 <112384093+Mehroj9111@users.noreply.github.com>
2023-07-22 18:24:37 +0500

    Update Client.cs

--- Crm/Entities/Client.cs
+++ Crm/Entities/Client.cs
@@ -11,11 +11,4 @@ public sealed class Client
     public string OrderName { get; internal set; }
     public string OrderPrice { get; internal set; }
 
-    // public void Validate()
-    //  {
-    //     if (string.IsNullOrEmpty(FirstName))
-    //     {
-    //         throw new ArgumentException("FirstName клиента не может быть пустым");
-    //     }
-    //  }
 }
