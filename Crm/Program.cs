0e377ca95ef2265eda8bdadc20fad8a00dd4c411
Mehroj9111 <112384093+Mehroj9111@users.noreply.github.com>
2023-07-27 15:24:55 +0500

    Update Program.cs

--- Crm/Program.cs
+++ Crm/Program.cs
@@ -30,14 +30,34 @@ void CreateClient()
     }
     System.Console.Write("LastName : ");
     string lastName = Console.ReadLine();
+     if (string.IsNullOrEmpty(lastName))
+    {
+        throw new ArgumentException("FirstName клиента не может быть пустым");
+    }
     System.Console.Write("MiddleName : ");
     string middleName = Console.ReadLine();
+    if (string.IsNullOrEmpty(middleName))
+    {
+        throw new ArgumentException("FirstName клиента не может быть пустым");
+    }
     System.Console.Write("Age : ");
     short age = short.Parse(Console.ReadLine());
+    if (short.IsNullOrEmpty(age))
+    {
+        throw new ArgumentException("FirstName клиента не может быть пустым");
+    }
     System.Console.Write("PassportNumber : ");
     string passportNumber = Console.ReadLine();
+    if (string.IsNullOrEmpty(passportNumber))
+    {
+        throw new ArgumentException("FirstName клиента не может быть пустым");
+    }
     System.Console.Write("Gernder : ");
     Gender gender = (Gender) int.Parse(Console.ReadLine());
+    if (Gender.IsNullOrEmpty(gender))
+    {
+        throw new ArgumentException("FirstName клиента не может быть пустым");
+    }
 
     Client newClient = clientService.CreateClient(
         firstName,
@@ -59,6 +79,10 @@ void CreateOrder()
     }
     System.Console.Write("Order Price : ");
     decimal orderPrice = Convert.ToInt32(Console.ReadLine());
+     if (decimal.IsNullOrEmpty(orderPrice))
+    {
+        throw new ArgumentException("OrderName клиента не может быть пустым");
+    }
 
     Order newOrder = orderService.CreateOrder(
         orderName,
